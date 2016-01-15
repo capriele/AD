376d43a7576a0bbd005170c16e9b92fde5aff30d
friether <fr@engineering-sciences.de>
2016-01-14 20:00:06 -0500

    viwer built removed

--- codebase/trunk/viewer/pod-build/CMakeFiles/DT2NfmgBBMVodNV29coUATlmnL8GEJgt.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/DT2NfmgBBMVodNV29coUATlmnL8GEJgt"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/DT2NfmgBBMVodNV29coUATlmnL8GEJgt.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
