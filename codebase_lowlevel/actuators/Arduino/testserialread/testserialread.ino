7a6d81e2726b61d8755d2fc47bda3c81df6676e1
friether <fr@engineering-sciences.de>
2016-02-01 15:08:04 -0500

    added simple test script to echo pwm comand sent from user on jetson to arduino

---
+++ codebase_lowlevel/actuators/Arduino/testserialread/testserialread.ino
@@ -0,0 +1,32 @@
+String IncomingData = "";
+String Temp = "";
+char var;
+int tmp;
+int pwm_out[4];
+
+void setup()
+{
+Serial.begin(115200);
+  
+  Serial.println("Ready");
+}
+
+void loop()
+{
+ if(Serial.available()>16)
+      {
+
+         while(Serial.read() != 'a'){}
+       
+         for(int i=0; i<4; i++){
+             tmp = (Serial.read() - '0');
+             tmp = 10*tmp + (Serial.read() - '0');
+             tmp = 10*tmp + (Serial.read() - '0');
+             tmp = 10*tmp + (Serial.read() - '0');      
+             if(tmp < 2001 && tmp > 999 )
+                  pwm_out[i] = tmp;
+         Serial.print(pwm_out[i]);         Serial.println();
+         }
+         
+      }
+}
